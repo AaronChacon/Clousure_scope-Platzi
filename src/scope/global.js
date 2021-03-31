3f3c6423aeaa8fa5e43e0e02c401cda6326457ea
Aaron Chacon <aaronchacon86@gmail.com>
2021-03-31 18:22:29 -0400

    Scope

---
+++ src/scope/global.js
@@ -0,0 +1,12 @@
+var hello = 'Hello';
+let world = 'Hello World';
+const helloWorld = 'Hello world!';
+
+const anotherFuntion = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFuntion()
+
