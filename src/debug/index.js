0976950d3b6286912e618dce02b60f9194fc05a0
Aaron Chacon <aaronchacon86@gmail.com>
2021-04-01 16:12:52 -0400

    end

---
+++ src/debug/index.js
@@ -0,0 +1,13 @@
+
+var a = 'hello';
+
+function hello() {
+    let b = 'hello world';
+    const c = 'hello World!';
+    if (true) {
+        const d = 'hello World!!';
+        debugger
+    }
+}
+
+hello();
---
+++ src/hoisting/index.js
@@ -0,0 +1,19 @@
+/* 
+
+a = 2;
+var a;
+console.log(a); // 2 
+
+
+console.log(a); 
+var a = 2 // undefined
+
+
+*/
+
+
+function nameOfDog(name) {
+    console.log(name);
+}
+
+nameOfDog('Coco')
